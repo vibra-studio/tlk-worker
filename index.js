ba9a3a146c0ab03d6ade98ce0a8587cb4c8831e6
Vibra Studio <60678180+vibra-studio@users.noreply.github.com>
2025-09-20 01:55:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,28 @@ async function handleRequest(request) {
   const language = url.searchParams.get('language') || 'en'
   const page = url.searchParams.get('page') || 1
 
-  const tmdbApiKey = '410692947e4353d68c7faa660d7ed023'
-
-  const tmdbUrl = `https://api.themoviedb.org/3/${path}?api_key=${tmdbApiKey}&language=${language}&page=${page}`
+  const tmdbUrl = `https://api.themoviedb.org/3/${path}?language=${language}&page=${page}`
 
   try {
-    const response = await fetch(tmdbUrl)
+    const response = await fetch(tmdbUrl, {
+      headers: {
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MTA2OTI5NDdlNDM1M2Q2OGM3ZmFhNjYwZDdlZDAyMyIsIm5iZiI6MTcxMDU2MTQwMS4zNTAwMDAxLCJzdWIiOiI2NWY1MTg3OWUwMzlmMTAxN2QwMmZmM2QiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.u-zbWauZLgbDLY2v4hEufNYaczuFts4bFdGS38cvL5o",
+        accept: "application/json"
+      }
+    })
     const data = await response.json()
     return new Response(JSON.stringify(data), {
-      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+      headers: { 
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*' 
+      }
     })
   } catch (err) {
     return new Response(JSON.stringify({ error: err.message }), {
-      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+      headers: { 
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*' 
+      },
       status: 500
     })
   }
